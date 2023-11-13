3512adf0f6cb39639e3675fa80f6b1f705a4537c
Adam Planko <adi.planko@gmail.com>
2023-11-13 17:46:36 +0100

    prettier2

--- js/bpDetector.js
+++ js/bpDetector.js
@@ -4,25 +4,33 @@
  * License: MIT, see file 'LICENSE'
  */
 const bootstrapDetectBreakpoint = function () {
-    // cache some values on first call
-    if (!this.breakpointValues) {
-        this.breakpointNames = ["xxl", "xl", "lg", "md", "sm", "xs"]
-        this.breakpointValues = []
-        const isPriorBS5 = !!window.getComputedStyle(document.documentElement).getPropertyValue('--breakpoint-sm')
-        const prefix = isPriorBS5 ? "--breakpoint-" : "--bs-breakpoint-"
-        for (const breakpointName of this.breakpointNames) {
-            const value = window.getComputedStyle(document.documentElement).getPropertyValue(prefix + breakpointName)
-            if(value) {
-                this.breakpointValues[breakpointName] = value
-            }
-        }
-    }
-    let i = this.breakpointNames.length
+  // cache some values on first call
+  if (!this.breakpointValues) {
+    this.breakpointNames = ["xxl", "xl", "lg", "md", "sm", "xs"];
+    this.breakpointValues = [];
+    const isPriorBS5 = !!window
+      .getComputedStyle(document.documentElement)
+      .getPropertyValue("--breakpoint-sm");
+    const prefix = isPriorBS5 ? "--breakpoint-" : "--bs-breakpoint-";
     for (const breakpointName of this.breakpointNames) {
-        i--
-        if (window.matchMedia("(min-width: " + this.breakpointValues[breakpointName] + ")").matches) {
-            return {name: breakpointName, index: i}
-        }
+      const value = window
+        .getComputedStyle(document.documentElement)
+        .getPropertyValue(prefix + breakpointName);
+      if (value) {
+        this.breakpointValues[breakpointName] = value;
+      }
     }
-    return null
-}
+  }
+  let i = this.breakpointNames.length;
+  for (const breakpointName of this.breakpointNames) {
+    i--;
+    if (
+      window.matchMedia(
+        "(min-width: " + this.breakpointValues[breakpointName] + ")"
+      ).matches
+    ) {
+      return { name: breakpointName, index: i };
+    }
+  }
+  return null;
+};
